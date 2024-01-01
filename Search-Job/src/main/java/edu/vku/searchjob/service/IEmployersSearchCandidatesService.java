b0a080a6e84e7026b71a33b4c367bff6509f1db8
VoVanVuong <vvvuong.19it2@vku.udn.vn>
2024-01-01 18:29:49 +0700

    add more code

---
+++ Search-Job/src/main/java/edu/vku/searchjob/service/IEmployersSearchCandidatesService.java
@@ -0,0 +1,19 @@
+package edu.vku.searchjob.service;
+
+import edu.vku.searchjob.entity.Cadidates;
+import edu.vku.searchjob.entity.Employers;
+import edu.vku.searchjob.entity.EmployersSearchCandidates;
+
+import java.util.List;
+
+public interface IEmployersSearchCandidatesService {
+    public EmployersSearchCandidates addEmployersSearchCandidates(EmployersSearchCandidates employersSearchCandidates);
+//    public List<EmployersSearchCandidates> listStatusFalse();
+//    public List<EmployersSearchCandidates> listStatusTrue();
+public List<EmployersSearchCandidates> listStatusFalse(Cadidates cadidates);
+    public List<EmployersSearchCandidates> listStatusTrue(Cadidates cadidates);
+    public void statusTrue(int id);
+    public void statusfalse(int id);
+    public List<EmployersSearchCandidates> listStatusAndEmployer(Employers employers);
+    public List<EmployersSearchCandidates> listStatusAndEmployerTrue(Employers employers);
+}
